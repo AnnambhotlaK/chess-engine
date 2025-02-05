9d4a2a62a5bc37f08df50c62dd9b12f124d97e99
AnnambhotlaK <110944729+AnnambhotlaK@users.noreply.github.com>
2025-02-05 12:44:58 -0500

    create helper bitboard funcs for slider pieces

--- bbc.c
+++ bbc.c
@@ -1,3 +1,9 @@
+/* ***********************************
+   ***********************************
+        BITBOARD CHESS ENGINE IN C
+   ***********************************
+   ***********************************/
+
 #include <stdio.h>
 
 /* ***********************************
@@ -12,6 +18,33 @@
 #define get_bit(bitboard, square) (bitboard & (1ULL << square))
 #define set_bit(bitboard, square) (bitboard |= (1ULL << square))
 #define pop_bit(bitboard, square) (get_bit(bitboard, square) ? (bitboard ^= (1ULL << square)) : 0)
+
+// count bits in bitboard
+static inline int count_bits(U64 bitboard)
+{
+    // bit counter 
+    int count = 0;
+
+    // consecutively reset least significant 1st bit
+    // while bitboard has bits turned on
+    while (bitboard) {
+        count++;
+        // reset bit
+        bitboard &= bitboard - 1;
+    }
+    
+    return count;
+}
+
+// get ls 1st bit index
+static inline int get_ls1b_index(U64 bitboard) {
+    // make sure bitboard != 0
+    if (bitboard) {
+        return count_bits(((bitboard & -bitboard) - 1));
+    }
+    // otherwise return illegal index
+    return -1;
+}
 
 /* ***********************************
             ENUM DEFINITIONS
@@ -92,17 +125,17 @@ enum
     white,
     black
 };
+const char* square_to_coordinates[] = {
+    "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
+    "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+    "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+    "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+    "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+    "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+    "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+    "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1",
+};
 
-/* Need for future
-"a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"
-"a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"
-"a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6"
-"a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5"
-"a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4"
-"a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3"
-"a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"
-"a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"
-*/
 
 // print bitboard function
 // for debugging
@@ -474,9 +507,13 @@ int main(void)
     // init occupancy bitboard
     U64 block = 0ULL;
     set_bit(block, d7);
-    set_bit(block, d3);
+    set_bit(block, d2);
+    set_bit(block, d1);
     set_bit(block, g4);
     set_bit(block, b4);
-    print_bitboard(block);
-    print_bitboard(rook_attacks_on_the_fly(d4, block));
+    printf("     index: %d\n     coordinate: %s\n", get_ls1b_index(block), square_to_coordinates[get_ls1b_index(block)]);
+    U64 test_bitboard = 0ULL;
+    set_bit(test_bitboard, get_ls1b_index(block));
+    print_bitboard(test_bitboard);
+    return 0;
 }
